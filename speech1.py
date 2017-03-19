08caa4df595a958a0d88f3e77bfae44f571ab8fe
K Sax <saxberg@email.arizona.edu>
2017-03-19 00:51:39 +0000

    Hey, sanitizing input is a good thing

--- speech1.py
+++ speech1.py
@@ -2,8 +2,10 @@
 # Simply a command-line interface, requires espeak to be installed
 
 import os
+import re
 
 def speechify(text):
 	# Non-blocking has issues with successive calls
 	#os.system('espeak -p50 -s120 '{}' >/dev/null 2> /dev/null &".format(text))
+	text = re.sub('[\'\[\]&*#^!]', '', text)
 	os.system("espeak '{}' >/dev/null 2> /dev/null ".format(text))
--- tweets.py
+++ tweets.py
@@ -4,36 +4,32 @@ import twitter
 import tokens
 from speech1 import speechify as say
 
-def mentions(api):
-	last_id = 0L 
-	while(True):
-		time.sleep(61)
-		if last_id != 0L:
-			msgs = api.GetMentions(count=5, since_id=last_id)
-		else:
-			msgs = api.GetMentions(count=5)
-		if len(msgs) == 0:
-			continue
-		for msg in [x.AsDict() for x in msgs[::-1]]:
-			say("Mention from {} says: {}".format(msg[u'sender'][u'name'], msg[u'text']))
-			
-		latest = msgs[0].AsDict()
-		last_id = latest[u'id']
+def mentions(api, last_id=0L):
+	if last_id != 0L:
+		msgs = api.GetMentions(count=5, since_id=last_id)
+	else:
+		msgs = api.GetMentions(count=5)
+	if len(msgs) == 0:
+		continue
+	for msg in [x.AsDict() for x in msgs[::-1]]:
+		say("Mention from {} says: {}".format(msg[u'sender'][u'name'], msg[u'text']))
+		
+	latest = msgs[0].AsDict()
+	last_id = latest[u'id']
+	return last_id
 
-def directMessages(api):
-	last_id = 0L 
-	while(True):
-		time.sleep(61)
-		if last_id != 0L:
-			msgs = api.GetDirectMessages(count=3, since_id=last_id)
-		else:
-			msgs = api.GetDirectMessages(count=3)
-		if len(msgs) == 0:
-			continue
-		for msg in [x.AsDict() for x in msgs[::-1]]:
-			say("Message from {} says: {}".format(msg[u'sender'][u'name'], msg[u'text']))
-		latest = msgs[0].AsDict()
-		last_id = latest[u'id']
+def directMessages(api, last_id=0L):
+	if last_id != 0L:
+		msgs = api.GetDirectMessages(count=3, since_id=last_id)
+	else:
+		msgs = api.GetDirectMessages(count=3)
+	if len(msgs) == 0:
+		continue
+	for msg in [x.AsDict() for x in msgs[::-1]]:
+		say("Message from {} says: {}".format(msg[u'sender'][u'name'], msg[u'text']))
+	latest = msgs[0].AsDict()
+	last_id = latest[u'id']
+	return last_id
 			
 if __name__== "__main__":
 	api = twitter.Api(
@@ -41,6 +37,16 @@ if __name__== "__main__":
 	consumer_secret=tokens.consumer_secret,
 	access_token_key=tokens.access_token_key,
 	access_token_secret=tokens.access_token_secret)
-	
 	#print(api.VerifyCredentials())
-	directMessages(api)
+	
+	# Rate limit for DM: 1/min
+	# Rate limit for Mentions: 4/min
+	last_dm = 0L
+	last_mention = 0L
+	dm_count = 0
+	while(True):
+		time.sleep(16)
+		mentions(api, last_mention)
+		if dm_count == 3:
+			directMessages(api, last_dm)
+		dm_count = (dm_count + 1) % 4
